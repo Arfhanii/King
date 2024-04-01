b18f838e0440a31daf5bea7655dc0a4bce74ac5d
Arfhanii <158020456+Arfhanii@users.noreply.github.com>
2024-04-01 12:30:15 +0530

    Update file.sh

--- file.sh
+++ file.sh
@@ -3,13 +3,16 @@ pipeline {
     stages {
         stage ('github stage') {
             steps {
-                 echo 'github_stage'
-                 echo "git commit is : ${GIT_COMMIT}"
-                 echo "git previous commit : ${GIT_PREVIOUS_COMMIT}"
-                 echo "git previous successful commit :${GIT_PREVIOUS_SUCCESSFUL_COMMIT}"
-                 echo "git branch : ${GIT_BRANCH}"
-                 echo "git url : ${GIT_URL}"
-                 stash echo "git local branch : ${GIT_LOCAL_BRANCH}"
+                script {
+                    echo 'github_stage'
+                    echo "git commit is : ${GIT_COMMIT}"
+                    echo "git previous commit : ${GIT_PREVIOUS_COMMIT}"
+                    echo "git previous successful commit :${GIT_PREVIOUS_SUCCESSFUL_COMMIT}"
+                    echo "git branch : ${GIT_BRANCH}"
+                    echo "git url : ${GIT_URL}"
+                    echo "git local branch : ${GIT_LOCAL_BRANCH}"
+                    stash 'myStash' // Stash any files you want to keep for later stages
+                }
             }
         }
     }
