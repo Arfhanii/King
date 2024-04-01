0c6eb09181155d61c8f774bcfbe574711bf96b45
Arfhanii <158020456+Arfhanii@users.noreply.github.com>
2024-04-01 12:01:31 +0530

    Update file.sh

--- file.sh
+++ file.sh
@@ -5,6 +5,7 @@ pipeline {
             steps {
                  echo 'github_stage'
                  echo "git commit is : ${GIT_COMMIT}"
+                 echo "git previous commit : ${GIT_PREVIOUS_COMMIT}"
                  echo "git previous successful commit :${GIT_PREVIOUS_SUCCESSFUL_COMMIT}"
                  echo "git branch : ${GIT_BRANCH}"
                  echo "git url : ${GIT_URL}"
